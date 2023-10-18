5fe6fc40a3873f5c6aa09847a15660b540c96b18
korenkovaelena <korenkovalena@gmail.com>
2023-10-18 15:30:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String sequence = readFromFile();
+        int count = countArrows(sequence);
+        writeToFile(count);
+        String arrow = ">>-->";
+    }
+    public static String readFromFile() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("Input.txt"));
+        String sequence = sc.nextLine();
+        return sequence;
+    }
+    public static void writeToFile(int count) throws IOException {
+        String s = String.valueOf(count);
+        Files.writeString(Path.of("OUTPUT.TXT"),s);
+    }
+    public static int countArrows(String sequence)  {
+        int count = 0;
+        int index1 = sequence.indexOf(">>-->");
+        int index2 = sequence.indexOf("<--<<");
+        if (index1 != -1 || index2 != -1) {
+            String s= sequence;
+            count = countArrow(sequence, ">>-->", count);
+            sequence = s;
+            count = countArrow(sequence, "<--<<", count);
+        }
+        return count;
+    }
+    public static int countArrow(String sequence, String arrow, int count)  {
+        int index = sequence.indexOf(arrow);
+        while (index != -1 ) {
+            index = sequence.indexOf(arrow);
+            if (index >= 0 ){
+                count++;
+                sequence = sequence.substring(index+1);
+            }
+        }
+        return  count;
+    }
+}
